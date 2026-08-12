786a51820cdba70d8d0995e93807fad1393b762e
Harini239 <vharini825@gmail.com>
2026-08-12 11:23:40 +0530

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -0,0 +1,18 @@
+# Last updated: 8/12/2026, 11:23:39 AM
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next, l1 = l1, l1.next
+            else:
+                tail.next, l2 = l2, l2.next
+            tail = tail.next
+        tail.next = l1 or l2
+        return dummy.next
