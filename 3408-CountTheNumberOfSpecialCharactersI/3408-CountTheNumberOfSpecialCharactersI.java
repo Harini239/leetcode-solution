34e949e30bdb19056824ea67cb8260cafa5586df
Harini239 <vharini825@gmail.com>
2026-08-12 11:17:16 +0530

    Create 3408-CountTheNumberOfSpecialCharactersI

---
+++ 3408-CountTheNumberOfSpecialCharactersI/3408-CountTheNumberOfSpecialCharactersI.java
@@ -0,0 +1,16 @@
+// Last updated: 8/12/2026, 11:17:15 AM
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        HashSet<Character>set=new HashSet<>();
+        for(char ch : word.toCharArray()){
+            set.add(ch);
+        }
+        int count=0;
+        for(char ch='a';ch<='z';ch++){
+            if(set.contains(ch)&&set.contains((char)(ch-'a'+'A'))){
+                count++;
+            }
+        }
+        return count;
+    }
+}
