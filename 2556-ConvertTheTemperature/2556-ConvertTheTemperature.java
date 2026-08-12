5981272a4fd2bc908ff2dfa21ec05a3ea8f65a74
Harini239 <vharini825@gmail.com>
2026-08-12 11:17:58 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,6 @@
+// Last updated: 8/12/2026, 11:17:57 AM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[] {celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
