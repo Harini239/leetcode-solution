1f6cd2ef2f8b2288e85bb5a07a53a83e6fe8785a
Harini239 <vharini825@gmail.com>
2026-08-12 11:21:58 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,19 @@
+// Last updated: 8/12/2026, 11:21:57 AM
+
+    class Solution {
+    public static boolean isPalindrome(String s) {
+       StringBuilder sb = new StringBuilder();
+
+       for(char c: s.toCharArray()){
+        if(Character.isLetterOrDigit(c)){
+            sb.append(Character.toLowerCase(c));
+        }
+       }
+       String s1 = sb.toString();
+       String s2 = sb.reverse().toString();
+
+       return s1.equals(s2);
+    }
+} 
+
+    
