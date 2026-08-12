802ad6aee1ca20befdd55231845ae203f8d4ce3f
Harini239 <vharini825@gmail.com>
2026-08-12 11:17:01 +0530

    Create 4057-TotalWavinessOfNumbersInRangeI

---
+++ 4057-TotalWavinessOfNumbersInRangeI/4057-TotalWavinessOfNumbersInRangeI.java
@@ -0,0 +1,18 @@
+// Last updated: 8/12/2026, 11:17:00 AM
+class Solution {
+    public int totalWaviness(int num1, int num2) {
+        int ans = 0;
+        for (int x = num1; x <= num2; x++) {
+            String s = String.valueOf(x);
+            for (int i = 1; i < s.length() - 1; i++) {
+                if ((s.charAt(i) > s.charAt(i - 1) &&
+                     s.charAt(i) > s.charAt(i + 1)) ||
+                    (s.charAt(i) < s.charAt(i - 1) &&
+                     s.charAt(i) < s.charAt(i + 1))) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
